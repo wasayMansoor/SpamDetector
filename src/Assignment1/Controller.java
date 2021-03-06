d9ab5db8728f7287e6bf5aa9505dd0463a393a0f
wasayMansoor <71033432+wasayMansoor@users.noreply.github.com>
2021-03-07 00:23:48 +0300

    1st commit

--- src/Assignment1/Controller.java
+++ src/Assignment1/Controller.java
@@ -1,4 +1,22 @@
 package Assignment1;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TableView<TestFile> table;
+    @FXML
+    private TableColumn<TestFile, String> FileName;
+    @FXML
+    private TableColumn<TestFile, String> ActualClass;
+    @FXML
+    private TableColumn<TestFile, Double> SpamProbability;
+    @FXML
+    private TextField Accuracy;
+    @FXML
+    private TextField Precision;
+
 }
